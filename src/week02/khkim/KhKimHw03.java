fbede03a4238e9c5f88f0200ba478c3e2a6244db
tanonkim <robbins4bos@gmail.com>
2022-09-09 22:06:54 +0900

    fix : logic and conversion in printing

--- src/week02/khkim/KhKimHw03.java
+++ src/week02/khkim/KhKimHw03.java
@@ -2,46 +2,82 @@ package week02.khkim;
 
 import java.util.Scanner;
 
-
 public class KhkimHw03 {
-	
-		private final static int 가위 = 0;
-		private final static int 바위 = 1;
-		private final static int 보 = 2;
 
 
 	public static void main(String[] args) {
-
-		int randomNum = (int) (Math.random() * 3); // 0~2
-
-		System.out.println("가위(0), 바위(1), 보(2)중에 하나를 입력해주세요.");
-		System.out.print("입력 : ");
-
 		Scanner scanner = new Scanner(System.in);
-		int userNum = scanner.nextInt();
 		
 		// 플레이어 패 입력 시작 
 		while(true) {
+
+			System.out.println("가위(0), 바위(1), 보(2)중에 하나를 입력해주세요.");
+			System.out.print("입력 : ");
+
+
+			try {
+				int randomNum = (int) (Math.random() * 3); // 0~2
+
+				int userNum = scanner.nextInt();
 				
 				if (userNum > 2){ // 선택지 외 숫자 입력 
 					System.out.println("선택지 외의 수입니다. 0~2 사이의 수를 입력해주세요.");
 					break;
 				}
 				else {
-					System.out.println("user : " + userNum);
-					System.out.println(" com : " + randomNum);
+					// 숫자 입력타입 => 해당하는 문자로 변
+					if (userNum == 0) {
+						String numName = Integer.toString(userNum);
+						numName = numName.replace("0", "가위");
+						System.out.println("user : " + numName);
+					}else if(userNum == 1) {
+						String numName = Integer.toString(userNum);
+						numName = numName.replace("1", "바위");
+						System.out.println("user : " + numName);
+					}else{
+						String numName = Integer.toString(userNum);
+						numName = numName.replace("2", "보");
+						System.out.println("user : " + numName);
+					}
 					
-					if (userNum - randomNum == 1) {
+					if (randomNum == 0) {
+						String numName = Integer.toString(randomNum);
+						numName = numName.replace("0", "가위");
+						System.out.println(" com : " + numName);
+					}else if(randomNum == 1) {
+						String numName = Integer.toString(randomNum);
+						numName = numName.replace("1", "바위");
+						System.out.println(" com : " + numName);
+					}else{
+						String numName = Integer.toString(randomNum);
+						numName = numName.replace("2", "보");
+						System.out.println(" com : " + numName);
+					}
+
+			
+					// 승패 조건 
+					if ((userNum - randomNum) == 1 || (userNum - randomNum) == -2 ) {
 						System.out.println("승");
 					}
-					else if (userNum - randomNum == 0) {
-						System.out.println("무승부");
-					}else {
+					else if ((userNum - randomNum) == -1 || (userNum - randomNum) == 2 ) {
 						System.out.println("패");
-					}break;
-				}
-		}
-		scanner.close();
-	}
+					}else {
+						System.out.println("무승부");
+					}
+				System.out.println();
 
+				}
+				continue;
+			} // 예외 처리 
+			catch (Exception e) {
+				scanner.nextLine();
+				System.out.println("잘못된 값이 입력되었습니다. 숫자 0~2사이의 값을 입력해주세요.");
+
+			}
+			System.out.println();
+
+		}					
+		scanner.close();
+
+	}
 }
